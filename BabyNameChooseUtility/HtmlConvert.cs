0ad7492282ed2852a51ed513ff4019be7ce655e8
muzizongheng <jgli_2008@sina.com>
2015-06-07 19:44:47 +0800

    Read epub to statistic frequent words

---
+++ BabyNameChooseUtility/HtmlConvert.cs
@@ -0,0 +1,275 @@
+using System.IO;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+namespace BabyNameChooseUtility
+{
+    public class HtmlToText
+    {
+        #region Public Methods
+
+        public string StripHTML(string source)
+        {
+            try
+            {
+                string result;
+
+                // Remove HTML Development formatting
+                // Replace line breaks with space
+                // because browsers inserts space
+                result = source.Replace("\r", " ");
+                // Replace line breaks with space
+                // because browsers inserts space
+                result = result.Replace("\n", " ");
+                // Remove step-formatting
+                result = result.Replace("\t", string.Empty);
+                // Remove repeating spaces because browsers ignore them
+                result = Regex.Replace(result,
+                    @"( )+", " ");
+
+                // Remove the header (prepare first by clearing attributes)
+                result = Regex.Replace(result,
+                    @"<( )*head([^>])*>", "<head>",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    @"(<( )*(/)( )*head( )*>)", "</head>",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    "(<head>).*(</head>)", string.Empty,
+                    RegexOptions.IgnoreCase);
+
+                // remove all scripts (prepare first by clearing attributes)
+                result = Regex.Replace(result,
+                    @"<( )*script([^>])*>", "<script>",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    @"(<( )*(/)( )*script( )*>)", "</script>",
+                    RegexOptions.IgnoreCase);
+                //result = System.Text.RegularExpressions.Regex.Replace(result,
+                //         @"(<script>)([^(<script>\.</script>)])*(</script>)",
+                //         string.Empty,
+                //         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    @"(<script>).*(</script>)", string.Empty,
+                    RegexOptions.IgnoreCase);
+
+                // remove all styles (prepare first by clearing attributes)
+                result = Regex.Replace(result,
+                    @"<( )*style([^>])*>", "<style>",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    @"(<( )*(/)( )*style( )*>)", "</style>",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    "(<style>).*(</style>)", string.Empty,
+                    RegexOptions.IgnoreCase);
+
+                // insert tabs in spaces of <td> tags
+                result = Regex.Replace(result,
+                    @"<( )*td([^>])*>", "\t",
+                    RegexOptions.IgnoreCase);
+
+                // insert line breaks in places of <BR> and <LI> tags
+                result = Regex.Replace(result,
+                    @"<( )*br( )*>", "\r",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    @"<( )*li( )*>", "\r",
+                    RegexOptions.IgnoreCase);
+
+                // insert line paragraphs (double line breaks) in place
+                // if <P>, <DIV> and <TR> tags
+                result = Regex.Replace(result,
+                    @"<( )*div([^>])*>", "\r\r",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    @"<( )*tr([^>])*>", "\r\r",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    @"<( )*p([^>])*>", "\r\r",
+                    RegexOptions.IgnoreCase);
+
+                // Remove remaining tags like <a>, links, images,
+                // comments etc - anything that's enclosed inside < >
+                result = Regex.Replace(result,
+                    @"<[^>]*>", string.Empty,
+                    RegexOptions.IgnoreCase);
+
+                // replace special characters:
+                result = Regex.Replace(result,
+                    @" ", " ",
+                    RegexOptions.IgnoreCase);
+
+                result = Regex.Replace(result,
+                    @"&bull;", " * ",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    @"&lsaquo;", "<",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    @"&rsaquo;", ">",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    @"&trade;", "(tm)",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    @"&frasl;", "/",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    @"&lt;", "<",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    @"&gt;", ">",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    @"&copy;", "(c)",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    @"&reg;", "(r)",
+                    RegexOptions.IgnoreCase);
+                // Remove all others. More can be added, see
+                // http://hotwired.lycos.com/webmonkey/reference/special_characters/
+                result = Regex.Replace(result,
+                    @"&(.{2,6});", string.Empty,
+                    RegexOptions.IgnoreCase);
+
+                // for testing
+                //System.Text.RegularExpressions.Regex.Replace(result,
+                //       this.txtRegex.Text,string.Empty,
+                //       System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+                // make line breaking consistent
+                result = result.Replace("\n", "\r");
+
+                // Remove extra line breaks and tabs:
+                // replace over 2 breaks with 2 and over 4 tabs with 4.
+                // Prepare first to remove any whitespaces in between
+                // the escaped characters and remove redundant tabs in between line breaks
+                result = Regex.Replace(result,
+                    "(\r)( )+(\r)", "\r\r",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    "(\t)( )+(\t)", "\t\t",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    "(\t)( )+(\r)", "\t\r",
+                    RegexOptions.IgnoreCase);
+                result = Regex.Replace(result,
+                    "(\r)( )+(\t)", "\r\t",
+                    RegexOptions.IgnoreCase);
+                // Remove redundant tabs
+                result = Regex.Replace(result,
+                    "(\r)(\t)+(\r)", "\r\r",
+                    RegexOptions.IgnoreCase);
+                // Remove multiple tabs following a line break with just one tab
+                result = Regex.Replace(result,
+                    "(\r)(\t)+", "\r\t",
+                    RegexOptions.IgnoreCase);
+                // Initial replacement target string for line breaks
+                string breaks = "\r\r\r";
+                // Initial replacement target string for tabs
+                string tabs = "\t\t\t\t\t";
+                for (int index = 0; index < result.Length; index++)
+                {
+                    result = result.Replace(breaks, "\r\r");
+                    result = result.Replace(tabs, "\t\t\t\t");
+                    breaks = breaks + "\r";
+                    tabs = tabs + "\t";
+                }
+
+                // That's it.
+                return result;
+            }
+            catch
+            {
+                return source;
+            }
+        }
+
+        public string Convert(string path)
+        {
+            var doc = new HtmlDocument();
+            doc.Load(path);
+
+            var sw = new StringWriter();
+            ConvertTo(doc.DocumentNode, sw);
+            sw.Flush();
+            return sw.ToString();
+        }
+
+        public string ConvertHtml(string html)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            var sw = new StringWriter();
+            ConvertTo(doc.DocumentNode, sw);
+            sw.Flush();
+            return sw.ToString();
+        }
+
+        public void ConvertTo(HtmlNode node, TextWriter outText)
+        {
+            string html;
+            switch (node.NodeType)
+            {
+                case HtmlNodeType.Comment:
+                    // don't output comments
+                    break;
+
+                case HtmlNodeType.Document:
+                    ConvertContentTo(node, outText);
+                    break;
+
+                case HtmlNodeType.Text:
+                    // script and style must not be output
+                    string parentName = node.ParentNode.Name;
+                    if ((parentName == "script") || (parentName == "style"))
+                        break;
+
+                    // get text
+                    html = ((HtmlTextNode) node).Text;
+
+                    // is it in fact a special closing node output as text?
+                    if (HtmlNode.IsOverlappedClosingElement(html))
+                        break;
+
+                    // check the text is meaningful and not a bunch of whitespaces
+                    if (html.Trim().Length > 0)
+                    {
+                        outText.Write(HtmlEntity.DeEntitize(html));
+                    }
+                    break;
+
+                case HtmlNodeType.Element:
+                    switch (node.Name)
+                    {
+                        case "p":
+                            // treat paragraphs as crlf
+                            outText.Write("\r\n");
+                            break;
+                    }
+
+                    if (node.HasChildNodes)
+                    {
+                        ConvertContentTo(node, outText);
+                    }
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void ConvertContentTo(HtmlNode node, TextWriter outText)
+        {
+            foreach (HtmlNode subnode in node.ChildNodes)
+            {
+                ConvertTo(subnode, outText);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ BabyNameChooseUtility/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace BabyNameChooseUtility
+{
+    internal class Program
+    {
+        private static readonly IDictionary<string, long> MostFrequentWords = new Dictionary<string, long>();
+
+        private static readonly IDictionary<string, long> MostFrequentReduplicatedWords =
+            new Dictionary<string, long>();
+
+        private static readonly char[] Separators =
+        {
+            ' ', ',', ';', '.', '!', '"', ':', '?',
+            '。', '！', '：', '，', '“', '”', '？',
+            '\r', '\n', '\t'
+        };
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please input your dictionary.");
+
+            string fileName = Console.ReadLine();
+            if (string.IsNullOrEmpty(fileName))
+            {
+                fileName = @"chuci.epub";
+            }
+
+            if (Directory.Exists(fileName))
+            {
+                string[] files = Directory.GetFiles(fileName);
+                foreach (string file in files)
+                {
+                    Console.WriteLine("Start to process : " + file);
+                    ProcessEpub(file);
+                }
+            }
+            else if (File.Exists(fileName))
+            {
+                Console.WriteLine("Start to process : " + fileName);
+                ProcessEpub(fileName);
+            }
+            else
+            {
+                Console.WriteLine("File path is not exit");
+            }
+
+            Console.WriteLine("Process finishied...");
+            Console.ReadKey();
+        }
+
+        private static void ProcessEpub(string fileName)
+        {
+            var stream = new MemoryStream();
+
+            using (FileStream file = File.OpenRead(fileName))
+            {
+                file.Seek(0, SeekOrigin.Begin);
+                file.CopyTo(stream);
+                stream.Seek(0, SeekOrigin.Begin);
+            }
+
+            using (var s = new ZipInputStream(stream))
+            {
+                ReadEpub(s);
+            }
+
+            using (FileStream s = File.Create(@"Most frequent words.txt"))
+            {
+                using (var sw = new StreamWriter(s))
+                {
+                    foreach (var w in MostFrequentWords.OrderByDescending(e => e.Value))
+                    {
+                        sw.WriteLine("{0} : {1}", w.Key, w.Value);
+                    }
+                    sw.Close();
+                }
+            }
+
+            using (FileStream s = File.Create(@"Most frequent duplicated words.txt"))
+            {
+                using (var sw = new StreamWriter(s))
+                {
+                    foreach (var w in MostFrequentReduplicatedWords.OrderByDescending(e => e.Value))
+                    {
+                        sw.WriteLine("{0} : {1}", w.Key, w.Value);
+                    }
+                }
+            }
+        }
+
+        private static void ReadEpub(ZipInputStream zisStream)
+        {
+            if (zisStream == null)
+            {
+                return;
+            }
+
+            ZipEntry theEntry;
+
+            while ((theEntry = zisStream.GetNextEntry()) != null)
+            {
+                Debug.WriteLine(theEntry.Name);
+
+                string directoryName = Path.GetDirectoryName(theEntry.Name);
+                string fileName = Path.GetFileName(theEntry.Name);
+                string fileExtension = Path.GetExtension(theEntry.Name);
+
+                if (!fileExtension.Equals(".xhtml") && !fileExtension.Equals(".html"))
+                {
+                    continue;
+                }
+
+#if true
+                using (var memoryStream = new MemoryStream())
+                {
+                    zisStream.CopyTo(memoryStream);
+
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+
+                    using (var sr = new StreamReader(memoryStream))
+                    {
+                        string tmpContent = sr.ReadToEnd();
+                        var htt = new HtmlToText();
+                        string text = htt.StripHTML(tmpContent);
+
+                        string[] lines = text.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
+
+                        foreach (string line in lines)
+                        {
+                            char[] words = line.ToCharArray();
+
+                            char preWord = '\x0000';
+                            foreach (char w in words)
+                            {
+                                if (!IsChineseWord(w))
+                                {
+                                    continue;
+                                }
+
+                                if (MostFrequentWords.ContainsKey(w.ToString()))
+                                {
+                                    MostFrequentWords[w.ToString()] = MostFrequentWords[w.ToString()] + 1;
+                                }
+                                else
+                                {
+                                    MostFrequentWords[w.ToString()] = 1;
+                                }
+
+                                if (preWord == w)
+                                {
+                                    string duplicatedWord = string.Format("{0}{0}", w);
+                                    if (MostFrequentReduplicatedWords.ContainsKey(duplicatedWord))
+                                    {
+                                        MostFrequentReduplicatedWords[duplicatedWord] =
+                                            MostFrequentReduplicatedWords[duplicatedWord] + 1;
+                                    }
+                                    else
+                                    {
+                                        MostFrequentReduplicatedWords[duplicatedWord] = 1;
+                                    }
+                                }
+
+                                preWord = w;
+                            }
+                        }
+                    }
+                }
+#else
+
+                #region CreateFiles
+                // create directory
+                if (!string.IsNullOrEmpty(directoryName) && directoryName.Length > 0)
+                {
+                    Directory.CreateDirectory(directoryName);
+                }
+
+                //create file
+                if (!string.IsNullOrEmpty(fileName))
+                {
+                    using (FileStream streamWriter = File.Create(theEntry.Name))
+                    {
+                        int size = 2048;
+                        var data = new byte[2048];
+                        while (true)
+                        {
+                            size = zisStream.Read(data, 0, data.Length);
+                            if (size > 0)
+                            {
+                                streamWriter.Write(data, 0, size);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                } 
+                #endregion
+#endif
+            }
+        }
+
+        private static bool IsChineseWord(char chr)
+        {
+            if (chr >= 0x4E00 && chr <= 0x9FFF)
+                return true;
+
+            return false;
+        }
+    }
+}
